81c62f53a4ffc8a6f1240dc27b5914945f257ec2
Krish Ray <raykrish25@gmail.com>
2024-08-02 22:22:49 +0530

    Added

---
+++ src/player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+@export var acceleration := 10.0
+@export var max_speed := 350.0
+@export var rotation_speed := 250.0
+
+func _physics_process(delta):
+	var input_vecor = Vector2(0,Input.get_axis("move_up","move_down"))
+	
+	velocity += input_vecor.rotated(rotation) * acceleration
+	velocity = velocity.limit_length(max_speed)
+	
+	if Input.is_action_pressed("rotate_right"):
+		rotate(deg_to_rad(rotation_speed*delta))
+	if Input.is_action_pressed("rotate_left"):
+		rotate(deg_to_rad(-rotation_speed*delta))
+		
+	if input_vecor.y ==0:
+		velocity = velocity.move_toward(Vector2.ZERO,3)
+		
+	move_and_slide()
+
+	var screen_size = get_viewport_rect().size
+	if global_position.y < 0:
+		global_position.y = screen_size.y
+	elif global_position.y > screen_size.y:
+		global_position.y = 0
+	if global_position.x < 0:
+		global_position.x = screen_size.x
+	elif global_position.x > screen_size.x:
+		global_position.x = 0
