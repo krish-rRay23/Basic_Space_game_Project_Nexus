a22178fd9ac28cb148a819e7adac52592e058fa7
vedant022 <150889439+vedant022@users.noreply.github.com>
2024-08-04 21:27:22 +0530

    added main menu
    added intial main menu with video background and normal buttons

---
+++ Endless_Survivor_Space_Combat/src/main-menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_play_game_pressed():
+	get_tree().change_scene_to_file("res://scenes/player.tscn")
+
+
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
+
+
+func _on_credits_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	get_tree().quit()
