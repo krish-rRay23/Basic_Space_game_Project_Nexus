8a02379c07c10cf52904f6cdb5ba33564276fa49
Krish Ray <raykrish25@gmail.com>
2024-08-10 22:58:24 +0530

    Updated

---
+++ Endless_Survivor_Space_Combat/src/main_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+func _on_start_game_pressed():
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
--- Endless_Survivor_Space_Combat/src/player.gd
+++ Endless_Survivor_Space_Combat/src/player.gd
@@ -1,29 +1,32 @@
 extends CharacterBody2D
 
 signal laser_shot(laser)
-@export var acceleration := 10.0
-@export var max_speed := 350.0
-@export var rotation_speed := 250.0
+@export var acceleration := 1500.0
+@export var max_speed := 400.0
+@export var friction := 600.0
+@export var rotation_speed := 1.5
 
 @onready var muzzle = $Muzzle
 
 var laser_scene = preload("res://scenes/laser.tscn")
 
 func _physics_process(delta):
-	var input_vecor = Vector2(0,Input.get_axis("move_up","move_down"))
+	var rotation_direction = Input.get_axis("rotate_left", "rotate_right")
+	var move_direction = Input.get_axis("move_down", "move_up")
 	
-	velocity += input_vecor.rotated(rotation) * acceleration
-	velocity = velocity.limit_length(max_speed)
+	var direction = Vector2(0, -1).rotated(rotation)
+	var target_velocity = direction * move_direction * max_speed
 	
-	if Input.is_action_pressed("rotate_right"):
-		rotate(deg_to_rad(rotation_speed*delta))
-	if Input.is_action_pressed("rotate_left"):
-		rotate(deg_to_rad(-rotation_speed*delta))
-		
-	if input_vecor.y ==0:
-		velocity = velocity.move_toward(Vector2.ZERO,3)
-		
-	move_and_slide()
+	if move_direction != 0:
+		velocity = velocity.move_toward(target_velocity, acceleration * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+	
+	if velocity.length() > 0:
+		velocity = velocity.limit_length(max_speed)
+		move_and_slide()
+	
+	rotation += rotation_direction * rotation_speed * delta
 
 	var screen_size = get_viewport_rect().size
 	if global_position.y < 0:
@@ -34,12 +37,13 @@ func _physics_process(delta):
 		global_position.x = screen_size.x
 	elif global_position.x > screen_size.x:
 		global_position.x = 0
-		
+
 func _process(delta):
 	if Input.is_action_just_pressed("shoot"):
 		shoot_laser()
+
 func shoot_laser():
 	var l = laser_scene.instantiate()
 	l.global_position = muzzle.global_position
 	l.rotation = rotation
-	emit_signal("laser_shot",l)
+	emit_signal("laser_shot", l)
